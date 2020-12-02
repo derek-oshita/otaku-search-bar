7af91d2f45e5c0dc32c1102e3fb19b1d45f927fc
Derek Oshita <65000424+zetsubou-kira@users.noreply.github.com>
2020-12-01 20:40:48 -0800

    JSON data showing up in console.

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-console.log('hello...')
+// HTML ELEMENTS 
+const searchBar = document.getElementById('searchBar');
+const searchButton = document.getElementsByClassName('search-btn'); 
+
+// DATA 
+const loadData = async () => {
+    fetch('https://raw.githubusercontent.com/manami-project/anime-offline-database/master/anime-offline-database.json')
+    .then(
+        function(response){
+            if (response.status !== 200)  {
+                console.log('Error:', response.status); 
+                return; 
+            }
+
+            response.json().then(function(data) {
+                console.log(data)
+            })
+        }
+    )
+    .catch(function(err) {
+        console.log(err)
+    })
+}
+
+// EVENT LISTENERS
+searchBar.addEventListener('keyup', (e) => {
+    console.log(e)
+})
+
+loadData(); 
+

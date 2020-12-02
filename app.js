124798ee1d9ed241fd1b152f2282435f85f35951
Derek Oshita <65000424+zetsubou-kira@users.noreply.github.com>
2020-12-01 20:58:07 -0800

    Added .data to line 25 in order to access the array.

--- app.js
+++ app.js
@@ -1,30 +1,33 @@
 // HTML ELEMENTS 
 const searchBar = document.getElementById('searchBar');
 const searchButton = document.getElementsByClassName('search-btn'); 
+let animeArr = []; 
 
 // DATA 
 const loadData = async () => {
     fetch('https://raw.githubusercontent.com/manami-project/anime-offline-database/master/anime-offline-database.json')
-    .then(
-        function(response){
-            if (response.status !== 200)  {
-                console.log('Error:', response.status); 
-                return; 
-            }
+    // .then(
+    //     function(response){
+    //         if (response.status !== 200)  {
+    //             console.log('Error:', response.status); 
+    //             return; 
+    //         }
 
-            response.json().then(function(data) {
-                console.log(data)
-            })
-        }
-    )
-    .catch(function(err) {
-        console.log(err)
-    })
+    //         response.json().then(function(data) {
+    //             console.log(data)
+    //         })
+    //     }
+    // )
+    // .catch(function(err) {
+    //     console.log(err)
+    // })
+    .then(response => response.json())
+    .then(data => console.log(data.data))
 }
 
 // EVENT LISTENERS
 searchBar.addEventListener('keyup', (e) => {
-    console.log(e)
+    console.log(e.target.value)
 })
 
 loadData(); 

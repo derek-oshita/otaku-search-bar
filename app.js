19f722736ca574d5837090bd17051cf8a972c4af
Derek Oshita <65000424+zetsubou-kira@users.noreply.github.com>
2020-12-01 22:12:47 -0800

    Console logging by title or tag.

--- app.js
+++ app.js
@@ -1,33 +1,42 @@
 // HTML ELEMENTS 
 const searchBar = document.getElementById('searchBar');
 const searchButton = document.getElementsByClassName('search-btn'); 
-let animeArr = []; 
+let animeData = []; 
 
 // DATA 
 const loadData = async () => {
     fetch('https://raw.githubusercontent.com/manami-project/anime-offline-database/master/anime-offline-database.json')
-    // .then(
-    //     function(response){
-    //         if (response.status !== 200)  {
-    //             console.log('Error:', response.status); 
-    //             return; 
-    //         }
-
-    //         response.json().then(function(data) {
-    //             console.log(data)
-    //         })
-    //     }
-    // )
-    // .catch(function(err) {
-    //     console.log(err)
-    // })
     .then(response => response.json())
-    .then(data => console.log(data.data))
+    .then(data => {
+        animeData = data.data; 
+        // display goes here
+        display(animeData)
+    })
+    .catch(err => {
+        console.log('Error: ', err)
+    })
+}; 
+
+// FUNCTIONS
+const display = animeList => {
+    const htmlString = animeList.map( series => {
+        return `${series.title}`
+    })
+    console.log(htmlString)
 }
 
 // EVENT LISTENERS
 searchBar.addEventListener('keyup', (e) => {
-    console.log(e.target.value)
+    const searchStr = e.target.value.toLowerCase(); 
+    const resultsList = animeData.filter( anime => {
+        return (
+            // title
+            anime.title.toLowerCase().includes(searchStr) ||
+            // tags 
+            anime.tags.includes(searchStr)
+        )
+    })
+    console.log(resultsList)
 })
 
 loadData(); 
